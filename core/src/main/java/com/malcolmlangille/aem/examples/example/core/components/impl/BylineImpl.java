2cce943c95c67aff28a5a6e0c3f57cc88ffcb59c
malcolmlangille <malcolmlangille@gmail.com>
2019-08-06 13:08:12 -0400

    biline updates

--- core/src/main/java/com/malcolmlangille/aem/examples/example/core/components/impl/BylineImpl.java
+++ core/src/main/java/com/malcolmlangille/aem/examples/example/core/components/impl/BylineImpl.java
@@ -13,6 +13,8 @@ import org.apache.sling.models.annotations.injectorspecific.OSGiService;
 import org.apache.sling.models.annotations.injectorspecific.Self;
 import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
 import org.apache.sling.models.factory.ModelFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.malcolmlangille.aem.examples.example.core.components.Byline;
 import com.adobe.cq.wcm.core.components.models.Image;
@@ -24,7 +26,8 @@ import com.adobe.cq.wcm.core.components.models.Image;
         defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL
 )
 public class BylineImpl implements Byline {
-    protected static final String RESOURCE_TYPE = "wknd/components/content/byline";
+    protected static final String RESOURCE_TYPE = "example/components/content/byline";
+    private final Logger logger = LoggerFactory.getLogger(getClass());
 
     @Self
     private SlingHttpServletRequest request;
@@ -47,11 +50,15 @@ public class BylineImpl implements Byline {
 
     @Override
     public String getName() {
+        logger.info("INFO:getName()");
+        logger.debug("DEBUG:getName()");
         return name;
     }
 
     @Override
     public List<String> getOccupations() {
+        logger.info("INFO:getOccupations()");
+        logger.debug("DEBUG:getOccupations()");
         if (occupations != null) {
             Collections.sort(occupations);
             return occupations;
@@ -62,6 +69,8 @@ public class BylineImpl implements Byline {
 
     @Override
     public boolean isEmpty() {
+        logger.info("INFO:isEmpty()");
+        logger.debug("DEBUG:isEmpty()");
         final Image image = getImage();
 
         if (StringUtils.isBlank(name)) {
